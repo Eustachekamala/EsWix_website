bc39fe5f17e05da1f71e1ae676baa753a5205a06
eustachekamala <eustachejared25@gmail.com>
2024-06-20 10:01:18 +0300

    we modify the file of html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2")
-h2.textContent = "EsWix Is a site that we can use to generate a diagram with the data of the table"
-document.querySelector("body").appendChild(h2);
+const h3 = document.createElement("h2")
+h3.textContent = "EsWix Is a site that we can use to generate a diagram with the data of the table"
+document.querySelector("body").appendChild(h3);
