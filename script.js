73e9b87b4df45a30a7428c256e6d3af640edb2a2
eustachekamala <eustachejared25@gmail.com>
2024-06-20 11:52:15 +0300

    update of the css file

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h3 = document.createElement("h2")
 h3.textContent = "EsWix Is a site that we can use to generate a diagram with the data of the table"
-document.getAnimations("container-dashboard").appendChild(h3);
+document.querySelector("body").appendChild(h3);
