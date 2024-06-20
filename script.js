c6da44e3fe8ac28a245c6baa4b376e7e5170052c
eustachekamala <eustachejared25@gmail.com>
2024-06-20 09:56:07 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.createElement("h2")
+h2.textContent = "EsWix Is a site that we can use to generate a diagram with the data of the table"
+document.querySelector("body").appendChild(h2);
